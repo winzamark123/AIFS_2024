c75a66d4ae0cb9b875452f163dfa5ca0b779f3d8
Win Cheng <winzamark12@gmail.com>
2024-05-19 01:37:55 -0700

    OpenAIAPI insufficient credit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,35 @@ import Image from 'next/image';
 export default function Home() {
   const { image, loading, uploadImage, response } = useUploadImage();
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [advice, setAdvice] = useState<string | null>(null);
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (selectedFile) {
-      uploadImage(selectedFile);
+      await uploadImage(selectedFile);
+
+      if (response) {
+        const res = await fetch('http://localhost:3000/api/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ condition: response }),
+        });
+        const data = await res.json();
+        console.log('data', data);
+        setAdvice(data.advice);
+      }
     }
   };
 
   return (
-    <main>
+    <main className="flex items-center justify-center">
       <div className="flex h-1/2 w-1/2 flex-col items-center gap-3 border border-slate-500">
         <ImageUploader onFileSelected={setSelectedFile} />
         {loading && <p>Loading...</p>}
         {image && <Image src={image} alt="Uploaded" />}
         {response && <p>{response}</p>}
+        {advice && <p>{advice}</p>}
         <Button onClick={handleSubmit}>Submit</Button>
       </div>
     </main>
