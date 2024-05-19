67d47fe882889fc681c3783fa731228f44184582
Win Cheng <winzamark12@gmail.com>
2024-05-18 17:22:28 -0700

    Theme ETC init works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-'use client'
-import ImageUploader from "@/components/ImageUploader"
+'use client';
+import ImageUploader from '@/components/ImageUploader';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
 
-export default function Home(){
+export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
-      <ImageUploader></ImageUploader>
+      <div className="flex h-1/2 w-1/2 flex-col items-center gap-3 border border-slate-500">
+        <ImageUploader />
+        <div className="flex h-1/3 w-1/2 flex-col items-center border border-black"></div>
+
+        <Textarea></Textarea>
+        <Button>Submit</Button>
+      </div>
     </main>
-  )
+  );
 }
